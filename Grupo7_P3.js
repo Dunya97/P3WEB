d56a31750da461eef8a2435134ce081a595b9b6b
Belén Canals <belen.canals.2015@salleurl.edu>
2018-11-19 23:23:52 +0100

    array

---
+++ Grupo7_P3.js
@@ -0,0 +1,33 @@
+var video = {};
+
+typeof video.title;
+typeof video.image;
+typeof video.link;
+typeof video.description;
+
+var a = new Array();
+
+video.title = "10 Times Disney Made HUGE Mistakes";
+video.image = "https://i.ytimg.com/vi/419mibN54H8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&amp;rs=AOn4CLAnvoMQbfbgTcu_gMpNVJkQupoE8Q";
+video.link = "https://www.youtube.com/watch?v=419mibN54H8";
+video.description = "Disney has mastered the art of storytelling by captivating audiences through its narrative for generations. Audiences have adored Disney movies for their love stories, friendships, music, and animation. Nevertheless, Disney is not immune to mistakes as we’ll see today as we go through 10 Times Disney Make HUGE Mistakes.Noticing a small mistake in a movie can really take away from its magical qualities! “The Emperor’s New Groove” has its setting mixed up while “The Lion King” didn’t do the best research when it came to Africa’s biodiversity. Turns out “Hercules” is much more fiction than myth! Meanwhile, “Frozen” includes an appearing sword with no explanation, Cinderella has a secret outfit change, and Aladdin is missing body parts! Some of our favorite Disney movies have mistakes that are impossible to ignore once you know about them! Watch until the end to see why several mistakes are offensive in “Pocahontas.";
+a.push(video);
+
+video.title = "10 Disney Princess Connections That Will Make Your Jaw Drop";
+video.image = "https://i.ytimg.com/vi/Ee2lH2jGmJM/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAPebZaZrL5I6RKYWdc51-p6r3jkw";
+video.link = "https://www.youtube.com/watch?v=Ee2lH2jGmJM";
+video.description = "";
+a.push(video);
+
+video.title = "10 Shocking Facts You Didn't Know About The Minions";
+video.image = "https://i.ytimg.com/vi/cnBrkS323Po/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD1FKOL95QOEdBovmAYs42gntciQA";
+video.link = "https://www.youtube.com/watch?v=cnBrkS323Po";
+video.description = "";
+a.push(video);
+
+video.title = "25 Nickelodeon Fan Theories That Will Ruin Your Childhood";
+video.image = "https://i.ytimg.com/vi/JQdtFUjMBHg/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBld_MZ4VCi0ezfwdrjdPL8uJLj3A";
+video.link = "https://www.youtube.com/watch?v=iy77e5_uNyk";
+video.description = "";
+a.push(video);
+console.log(a);
