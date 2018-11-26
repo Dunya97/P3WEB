f2547d6430fbaf2833ab9a841e6f2cbd647ea376
Belén Canals <belen.canals.2015@salleurl.edu>
2018-11-26 21:57:23 +0100

    search

--- Grupo7_P3.js
+++ Grupo7_P3.js
@@ -1,12 +1,87 @@
-var arrayVideos = new Array();
+let arrayVideos = [];
 
-document.addEventListener('DOMContentLoaded', function() {
+
+
+window.onload = function() {
     addVideosArray();
+    const button = document.getElementById("search");
+    Listener.add(button, "click", Listener.eventSearch, true);
+};
+
+const Listener = {
+    add: function add(object, event, callback, capture) {
+        object.addEventListener(event,callback,capture);
+    },
+    eventSearch: function(event) {
+        resetResults();
+        event.preventDefault();
+        const inputValue = document.getElementById("searchInput").value;
+        if (inputValue) {
+            for (let i = 0; i < arrayVideos.length; i++) {
+                if (arrayVideos[i].title.toLowerCase().includes(inputValue.toLowerCase())) {
+                    showSearchResults(i);
+                }
+            }
+        }
+        else {
+            alert("Debes introducir texto para poder buscar")
+        }
+    }
+};
+
+function resetResults() {
+    let sectionResults = document.getElementById("results");
+    sectionResults.parentNode.removeChild(sectionResults);
+    let firstSection = document.getElementById("firstSection");
+    let addSection = document.createElement('section');
+    addSection.class = 'flex';
+    addSection.id = 'results';
+    firstSection.insertBefore(addSection, firstSection.children[1]);
     
+    addSection.style.display = "flex";
+    addSection.style.flexDirection = "row";
+    addSection.style.flexWrap = "wrap";
+    addSection.style.justifyContent = "flex-start";
+}
 
-});
+function showSearchResults(index) {
 
-function Video(title, image, link, description, type){
+    let sectionResults = document.getElementById("results");
+    
+    let descriptionResult = document.createElement("p");
+    let nodeD = document.createTextNode(arrayVideos[index].description);
+    descriptionResult.appendChild(nodeD);
+
+    let titleResults = document.createElement('h3');
+    let nodeT = document.createTextNode(arrayVideos[index].title);
+    titleResults.appendChild(nodeT);
+
+    let figCaption = document.createElement('figcaption');
+    figCaption.appendChild(titleResults);
+    figCaption.appendChild(descriptionResult);
+
+    let imageResults = document.createElement('img');
+    imageResults.src = arrayVideos[index].image;
+    imageResults.alt ="video youtube";
+    imageResults.width = "510";
+
+    let linkResults = document.createElement('a');
+    linkResults.href = arrayVideos[index].link;
+    linkResults.title = "Go to video";
+    linkResults.appendChild(imageResults);
+
+    let figureResults = document.createElement('figure');
+    figureResults.appendChild(linkResults);
+    figureResults.appendChild(figCaption);
+
+    let article = document.createElement('article');
+    article.appendChild(figureResults);
+
+    sectionResults.appendChild(article);
+    
+}
+
+function Video(title, image, link, description, type) {
     this.title = title;
     this.image = image;
     this.link = link;
@@ -15,24 +90,18 @@ function Video(title, image, link, description, type){
 }
 
 function addVideosArray() {
+    const titles = ["10 Times Disney Forgot How To Disney", "10 Disney Princess Connections That Will Make Your Jaw Drop", "10 Shocking Facts You Didn't Know About The Minions", "25 Nickelodeon Fan Theories That Will Ruin Your Childhood", "How to stay calm when you know you'll be stressed | Daniel Levitin", "The psychology of self-motivation | Scott Geller", "How language shapes the way we think | Lera Boroditsky", "10 ways to have a better conversation | Celeste Headlee"];
+    const images = ["https://i.ytimg.com/vi/L_V8fS3Z-gI/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD7FJV8T7tMTofIvyC8Od7O9y4OUQ", "https://i.ytimg.com/vi/Ee2lH2jGmJM/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAPebZaZrL5I6RKYWdc51-p6r3jkw", "https://i.ytimg.com/vi/cnBrkS323Po/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD1FKOL95QOEdBovmAYs42gntciQA", "https://i.ytimg.com/vi/JQdtFUjMBHg/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBld_MZ4VCi0ezfwdrjdPL8uJLj3A", "https://i.ytimg.com/vi/8jPQjjsBbIc/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDdFPYicErnlk182Jx1GTSER6uNyg", "https://i.ytimg.com/vi/7sxpKhIbr0E/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD4HOuwpAdYYXGORy2i75wy26VGLw", "https://i.ytimg.com/vi/RKK7wGAYP6k/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBzhJvH09UUbzM2_6IHw-0vtZDf0A", "https://i.ytimg.com/vi/R1vskiVDwl4/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCiGNth8Tv-us_xIdSo9CHjfobQ6w"];
+    const links = ["https://www.youtube.com/watch?v=L_V8fS3Z-gI", "https://www.youtube.com/watch?v=Ee2lH2jGmJM", "https://www.youtube.com/watch?v=cnBrkS323Po", "https://www.youtube.com/watch?v=iy77e5_uNyk", "https://www.youtube.com/watch?v=8jPQjjsBbIc",  "https://www.youtube.com/watch?v=7sxpKhIbr0E", "https://www.youtube.com/watch?v=RKK7wGAYP6k", "https://www.youtube.com/watch?v=R1vskiVDwl4"];
+    const descriptions = ["", "", "", "", "", "", "", ""];
+    const types = ["Videos más vistos", "Videos más vistos", "Videos más vistos", "Videos más vistos", "Videos más populares", "Videos más populares", "Videos más populares", "Videos más populares"];
 
-    var titles = ["10 Times Disney Made HUGE Mistakes", "10 Disney Princess Connections That Will Make Your Jaw Drop", "10 Shocking Facts You Didn't Know About The Minions", "25 Nickelodeon Fan Theories That Will Ruin Your Childhood", "How to stay calm when you know you'll be stressed | Daniel Levitin", "The psychology of self-motivation | Scott Geller", "How language shapes the way we think | Lera Boroditsky", "10 ways to have a better conversation | Celeste Headlee"];
-    var images = ["https://i.ytimg.com/vi/419mibN54H8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&amp;rs=AOn4CLAnvoMQbfbgTcu_gMpNVJkQupoE8Q", "https://i.ytimg.com/vi/Ee2lH2jGmJM/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAPebZaZrL5I6RKYWdc51-p6r3jkw", "https://i.ytimg.com/vi/cnBrkS323Po/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD1FKOL95QOEdBovmAYs42gntciQA", "https://i.ytimg.com/vi/JQdtFUjMBHg/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBld_MZ4VCi0ezfwdrjdPL8uJLj3A", "https://i.ytimg.com/vi/8jPQjjsBbIc/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDdFPYicErnlk182Jx1GTSER6uNyg", "https://i.ytimg.com/vi/7sxpKhIbr0E/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD4HOuwpAdYYXGORy2i75wy26VGLw", "https://i.ytimg.com/vi/RKK7wGAYP6k/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBzhJvH09UUbzM2_6IHw-0vtZDf0A", "https://i.ytimg.com/vi/R1vskiVDwl4/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCiGNth8Tv-us_xIdSo9CHjfobQ6w"];
-    var links = ["https://www.youtube.com/watch?v=419mibN54H8", "https://www.youtube.com/watch?v=Ee2lH2jGmJM", "https://www.youtube.com/watch?v=cnBrkS323Po", "https://www.youtube.com/watch?v=iy77e5_uNyk", "https://www.youtube.com/watch?v=8jPQjjsBbIc",  "https://www.youtube.com/watch?v=7sxpKhIbr0E", "https://www.youtube.com/watch?v=RKK7wGAYP6k", "https://www.youtube.com/watch?v=R1vskiVDwl4"];
-    var descriptions = ["Disney has mastered the art of storytelling by captivating audiences through its narrative for generations. Audiences have adored Disney movies for their love stories, friendships, music, and animation. Nevertheless, Disney is not immune to mistakes as we’ll see today as we go through 10 Times Disney Make HUGE Mistakes.Noticing a small mistake in a movie can really take away from its magical qualities! “The Emperor’s New Groove” has its setting mixed up while “The Lion King” didn’t do the best research when it came to Africa’s biodiversity. Turns out “Hercules” is much more fiction than myth! Meanwhile, “Frozen” includes an appearing sword with no explanation, Cinderella has a secret outfit change, and Aladdin is missing body parts! Some of our favorite Disney movies have mistakes that are impossible to ignore once you know about them! Watch until the end to see why several mistakes are offensive in “Pocahontas.", "", "", "", "", "", "", ""];
-    var types = ["Videos más vistos", "Videos más vistos", "Videos más vistos", "Videos más vistos", "Videos más populares", "Videos más populares", "Videos más populares", "Videos más populares"];
-
-    for(var i = 0; i < titles.length; i++) {
-        var video = new Video(titles[i], images[i], links[i], descriptions[i], types[i]);
-        arrayVideos.push(video);
+    for (let i = 0; i < titles.length; i++) {
+        arrayVideos.push(new Video(titles[i], images[i], links[i], descriptions[i], types[i]));
     }
 }
 
-function createImageContext(){
-    var generateSection = document.createElement('section');
-    generateSection.classList.add('flex');
-}
-function show_image(src, width, height, alt) {
+/*function showImage(src, width, height, alt) {
     var img = document.createElement("img");
     img.src = src;
     img.width = width;
@@ -41,4 +110,4 @@ function show_image(src, width, height, alt) {
 
     // This next line will just add it to the <body> tag
     document.body.appendChild(img);
-}
+}*/
