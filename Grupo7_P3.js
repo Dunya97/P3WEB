304c23d07e0b3aa50c7768042aeef5e279b9337d
Dunya97 <dunya.mohamed.2015@salleurl.edu>
2018-11-28 00:16:04 +0100

    API

--- Grupo7_P3.js
+++ Grupo7_P3.js
@@ -1,11 +1,70 @@
-let arrayVideos = [];
+var arrayVideos = [];
+var arrayId = new Array();
+var videosTop = new Array();
+var toptitles = [];
+var topimages = [];
+var toplinks = [];
+var topdescription = [];
+var toptypes = [];
+var API_KEY = "AIzaSyDf2xNoDJVH3RNuVF_GeHQadktM3leyGDo";
+
+function reqListener2 () {
+    let object = JSON.parse(this.responseText);
+    toptitles.push(object.items[0].snippet.localized.title);
+    //topimages[topimages.length] = object.items[0].snippet.thumbnails.default.url;
+    topimages.push(object.items[0].snippet.thumbnails.high.url);
+    toplinks.push("https://www.youtube.com/watch?v="+object.items[0].id);
+    topdescription.push(object.items[0].snippet.description);
+    toptypes.push("Videos top");
+    console.log(object.items[0].snippet.thumbnails);
+}
+
+var ajaxASYNC = {
+    request : function request(url){
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", reqListener);
+        xhr.open("GET", url, true);
+        xhr.send();
+    },
+    request2 : function request2(url) {
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", reqListener2);
+        xhr.open("GET", url, true);
+        xhr.send();
+    }
+
+};
+
+function reqListener () {
+    console.log("reqListener"+topimages[0]);    
+    let object = JSON.parse(this.responseText);
+
+    for (var i = 1; i < 5; i++) {
+        ajaxASYNC.request2("https://www.googleapis.com/youtube/v3/videos?part=snippet&id="+object.items[i].id+"&key="+API_KEY);
+        console.log("for"+ topimages);
+    } 
+    
+    setTimeout(function(){  addVideosArray( );},750);
+    setTimeout(function(){  showVideos();},1000); 
+
+}
+
 
 window.onload = function() {
-    addVideosArray();
-    showVideos();
+
+    
+    
+    ajaxASYNC.request("https://www.googleapis.com/youtube/v3/videos?part=statistics&chart=mostPopular&key="+API_KEY);
+            
+
+
+
+    
     const button = document.getElementById("search");
     Listener.add(button, "click", Listener.eventSearch, true);
 };
+
+
 
 const Listener = {
     add: function add(object, event, callback, capture) {
@@ -88,16 +147,26 @@ function Video(title, image, link, description, type) {
     this.type = type;
 }
 
+
+
 function addVideosArray() {
+    
     const titles = ["10 Times Disney Forgot How To Disney", "10 Disney Princess Connections That Will Make Your Jaw Drop", "10 Shocking Facts You Didn't Know About The Minions", "25 Nickelodeon Fan Theories That Will Ruin Your Childhood", "How to stay calm when you know you'll be stressed | Daniel Levitin", "The psychology of self-motivation | Scott Geller", "How language shapes the way we think | Lera Boroditsky", "10 ways to have a better conversation | Celeste Headlee"];
     const images = ["https://i.ytimg.com/vi/L_V8fS3Z-gI/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD7FJV8T7tMTofIvyC8Od7O9y4OUQ", "https://i.ytimg.com/vi/Ee2lH2jGmJM/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAPebZaZrL5I6RKYWdc51-p6r3jkw", "https://i.ytimg.com/vi/cnBrkS323Po/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD1FKOL95QOEdBovmAYs42gntciQA", "https://i.ytimg.com/vi/JQdtFUjMBHg/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBld_MZ4VCi0ezfwdrjdPL8uJLj3A", "https://i.ytimg.com/vi/8jPQjjsBbIc/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDdFPYicErnlk182Jx1GTSER6uNyg", "https://i.ytimg.com/vi/7sxpKhIbr0E/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD4HOuwpAdYYXGORy2i75wy26VGLw", "https://i.ytimg.com/vi/RKK7wGAYP6k/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBzhJvH09UUbzM2_6IHw-0vtZDf0A", "https://i.ytimg.com/vi/R1vskiVDwl4/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCiGNth8Tv-us_xIdSo9CHjfobQ6w"];
     const links = ["https://www.youtube.com/watch?v=L_V8fS3Z-gI", "https://www.youtube.com/watch?v=Ee2lH2jGmJM", "https://www.youtube.com/watch?v=cnBrkS323Po", "https://www.youtube.com/watch?v=iy77e5_uNyk", "https://www.youtube.com/watch?v=8jPQjjsBbIc",  "https://www.youtube.com/watch?v=7sxpKhIbr0E", "https://www.youtube.com/watch?v=RKK7wGAYP6k", "https://www.youtube.com/watch?v=R1vskiVDwl4"];
     const descriptions = ["", "", "", "", "", "", "", ""];
     const types = ["Videos más vistos", "Videos más vistos", "Videos más vistos", "Videos más vistos", "Videos más populares", "Videos más populares", "Videos más populares", "Videos más populares"];
 
+
     for (let i = 0; i < titles.length; i++) {
         arrayVideos.push(new Video(titles[i], images[i], links[i], descriptions[i], types[i]));
     }
+    
+    for(let i = 0; i<4; i++){
+        arrayVideos.push(new Video(toptitles[i], topimages[i], toplinks[i], topdescription[i], toptypes[i]));
+    }
+    //console.log(arrayVideos);
+
 }
 
 /*"use strict";
@@ -141,6 +210,10 @@ function showVideos() {
         else if(arrayVideos[i].type == "Videos más populares"){
             let sectionPopulares = document.getElementById("videosPopulares");
             createVideo(i, sectionPopulares);
+        }else if(arrayVideos[i].type == "Videos top"){
+            let sectionTop = document.getElementById("topVideos");
+            createVideo(i, sectionTop);
+            
         }
     }
 }
